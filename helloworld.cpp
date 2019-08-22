392b977a14bc6eee1fc21952deeeddaec6deac3c
tony coding hood <39744988+tonyCodingHood@users.noreply.github.com>
2019-08-22 15:10:50 +0300

    Update helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,9 +1,29 @@
 #include<iostream>
-
-int main(int argc, char *argv[]){
-   int a=0,b=9,c=10,k;
-   k=a+b+c;
-   std::cout<<"a+b+c="<<k<<std::endl;
-   std::cout<<k;
+#include<string>
+#include <bits/stdc++.h>
+    //isogram has no repeated letters
+    std::string is_isogram(std::string str) 
+{ 
+    int len = str.length(); 
+  
+    // Convert the string in lower case letters 
+    for (int i = 0; i < len; i++) 
+        str[i] = tolower(str[i]); 
+  
+    sort(str.begin(), str.end()); 
+  
+    for (int i = 0; i < len; i++) { 
+        if (str[i] == str[i + 1]) 
+            return "False"; 
+    } 
+    return "True"; 
+}
+    int main(int argc, char *argv[]){
+       std::string str1 ="Machine"; 
+    std::cout << is_isogram(str1) <<std:: endl;
+     std::string str4 = "Alphabet"; 
+    std::cout << is_isogram(str4) << std::endl; 
+  
+  
 
 }
